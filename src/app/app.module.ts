adca553c827a5244c58eb6743e37f21113f5199c
Julio Melchor <jcmelchorp@gmail.com>
2020-09-16 22:36:20 -0500

    Google Calendar API for RDS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDk1qgWOj6c2woDFxSbPdayrpUVFv97Ejg',
+  authDomain: 'rds-firebaseui.firebaseapp.com',
+  databaseURL: 'https://rds-firebaseui.firebaseio.com',
+  projectId: 'rds-firebaseui',
+  storageBucket: 'rds-firebaseui.appspot.com',
+  messagingSenderId: '382142356931',
+  appId: '1:382142356931:web:729d21482f39ccc2093176',
+  measurementId: 'G-FKR8NGWM3R'
+};
+
 
 @NgModule({
   declarations: [
@@ -10,7 +25,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
