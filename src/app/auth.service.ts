2fc9ba161be81ea20d9d98c3496a530eba91e38a
Julio C. Melchor P <jcmelchorp@gmail.com>
2020-11-14 13:09:45 -0600

    change to classroom API

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/auth';
+
+import { Observable } from 'rxjs';
+
 import { auth } from 'firebase/app';
+
+import { env } from 'process';
+
+import { environment } from './../environments/environment.prod';
 
 declare var gapi: any;
 
@@ -11,6 +17,7 @@ declare var gapi: any;
 export class AuthService {
   user$: Observable<firebase.User>;
   calendarItems: any[];
+  courses$: Observable<any[]>;
 
   constructor(public afAuth: AngularFireAuth) {
     this.initClient();
@@ -21,57 +28,68 @@ export class AuthService {
     gapi.load('client', () => {
       console.log('loaded client');
       gapi.client.init({
-        apiKey: 'AIzaSyDk1qgWOj6c2woDFxSbPdayrpUVFv97Ejg',
-        clientId: '382142356931-etv8n7tsj90gq9vloitis46vtr6mbqtt.apps.googleusercontent.com',
-        discoveryDocs: 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest',
-        scope: 'email profile https://www.googleapis.com/auth/calendar'
+        apiKey: environment.firebaseConfig.apiKey,
+        clientId: environment.firebaseConfig.clientId,
+        discoveryDocs: environment.firebaseConfig.discoveryDocs,
+        scope: environment.firebaseConfig.scope
       });
-      gapi.client.load('calendar', 'v3', () => console.log('loaded calendar'));
+      gapi.client.load('classroom', 'v1', () => console.log('loaded classroom'));
     })
   }
-
   async login() {
     const googleAuth = gapi.auth2.getAuthInstance();
     const googleUser = await googleAuth.signIn();
-    const token = googleUser.getAuthResponse().id_token;
-    console.log(googleUser);
-    const credential = auth.GoogleAuthProvider.credential(token);
-    await this.afAuth.signInAndRetrieveDataWithCredential(credential);
+    const authResponse = googleUser.getAuthResponse(true);
+    const credential = auth.GoogleAuthProvider.credential(authResponse.id_token, authResponse.access_token);
+    return this.afAuth.signInWithCredential(credential);
   }
+
 
   logout() {
     this.afAuth.signOut();
   }
 
-  async getCalendar() {
-    const events = await gapi.client.calendar.events.list({
-      calendarId: 'primary',
-      timeMin: new Date().toISOString(),
-      showDeleted: false,
-      singleEvents: true,
-      maxResults: 10,
-      orderBy: 'startTime',
-    });
-    console.log(events);
-    this.calendarItems = events.result.items;
+  async listTeachers(cid: string) {
+    const response = await gapi.client.classroom.courses.teachers.list({ courseId: cid });
+    const result = response.result;
+    const res = result.teachers;
+    return res;
   }
 
-  async insertEvent() {
-    const insert = await gapi.client.calendar.events.insert({
-      calendarId: 'primary',
-      start: {
-        dateTime: hoursFromNow(2),
-        timeZone: 'America/Mexico_City'
-      },
-      end: {
-        dateTime: hoursFromNow(3),
-        timeZone: 'America/Mexico_City'
-      },
-      summary: 'Have Fun!',
-      description: 'This is a test from an Angular WebApp'
-    });
-    await this.getCalendar();
+  async getTeacherProfile(uid: string) {
+    const response = await gapi.client.classroom.userProfiles.get({ userId: uid });
+    const result = response.result;
+    const res = result;
+    return res;
   }
 
+  async listCourses() {
+    const response = await gapi.client.classroom.courses.list();
+    const result = response.result;
+    const data = result.courses.map(course => {
+      let ownerName = gapi.client.classroom.courses.teachers.get({ courseId: course.id, userId: course.ownerId }).then((profile) => {
+        return ownerName = profile.profile.name.fullName;
+      });
+      let teacherGroup = this.listTeachers(course.id).then(teachers => {
+        return teacherGroup = teachers;
+      });
+      return {
+        courseId: course.id,
+        courseName: course.name,
+        section: course.section,
+        room: course.room,
+        desc: course.description,
+        descHeading: course.descriptionHeading,
+        creationDate: course.creationTime,
+        lastUpdate: course.updateTime,
+        guardiansEnable: course.guardiansEnabled,
+        alternateLink: course.alternateLink,
+        ownerName: ownerName,
+        teachersList: teacherGroup,
+      };
+    })
+    console.log(data);
+    this.courses$ = data;
+    return data;
+  }
 }
-const hoursFromNow = (n) => new Date(Date.now() + n * 1000 * 60 * 60).toISOString();
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,88 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC26297xuaMm1k9O7gcwbHDTryjMr8IDNI',
+    authDomain: 'proyecto-calli.firebaseapp.com',
+    databaseURL: 'https://proyecto-calli.firebaseio.com',
+    projectId: 'proyecto-calli',
+    storageBucket: 'proyecto-calli.appspot.com',
+    messagingSenderId: '778108325369',
+    appId: '1:778108325369:web:f9727f1ea9bca74a7c8128',
+    measurementId: 'G-RCWFBBG3HQ',
+    clientId:
+      '778108325369-a31ggkimq8qjkv4lgnojlnsc260qmtgs.apps.googleusercontent.com',
+    discoveryDocs: [
+      'https://classroom.googleapis.com/$discovery/rest?version=v1',
+    ],
+    scope: [
+      'email',
+      'profile',
+      'openid',
+      // View and manage announcements in Google Classroom
+      // 'https://www.googleapis.com/auth/classroom.announcements',
+
+      // View announcements in Google Classroom
+      'https://www.googleapis.com/auth/classroom.announcements.readonly',
+
+      // Manage your Google Classroom classes
+      // 'https://www.googleapis.com/auth/classroom.courses',
+
+      /*   // View your Google Classroom classes */
+      'https://www.googleapis.com/auth/classroom.courses.readonly',
+
+      // Manage your course work and view your grades in Google Classroom
+      // 'https://www.googleapis.com/auth/classroom.coursework.me',
+
+      // View your course work and grades in Google Classroom
+      'https://www.googleapis.com/auth/classroom.coursework.me.readonly',
+
+      // Manage course work and grades for students in the Google Classroom classes you teach and view the course work and grades for classes you administer
+      // 'https://www.googleapis.com/auth/classroom.coursework.students',
+
+      // View course work and grades for students in the Google Classroom classes you teach or administer
+      'https://www.googleapis.com/auth/classroom.coursework.students.readonly',
+
+      // See, edit, and create classwork materials in Google Classroom
+      // 'https://www.googleapis.com/auth/classroom.courseworkmaterials',
+
+      // See all classwork materials for your Google Classroom classes
+      'https://www.googleapis.com/auth/classroom.courseworkmaterials.readonly',
+
+      // View your Google Classroom guardians
+      'https://www.googleapis.com/auth/classroom.guardianlinks.me.readonly',
+
+      // View and manage guardians for students in your Google Classroom classes
+      // 'https://www.googleapis.com/auth/classroom.guardianlinks.students',
+
+      // View guardians for students in your Google Classroom classes
+      'https://www.googleapis.com/auth/classroom.guardianlinks.students.readonly',
+
+      // View the email addresses of people in your classes
+      'https://www.googleapis.com/auth/classroom.profile.emails',
+
+      // View the profile photos of people in your classes
+      'https://www.googleapis.com/auth/classroom.profile.photos',
+
+      // Receive notifications about your Google Classroom data
+      // 'https://www.googleapis.com/auth/classroom.push-notifications',
+
+      // Manage your Google Classroom class rosters
+      // 'https://www.googleapis.com/auth/classroom.rosters',
+
+      // View your Google Classroom class rosters
+      'https://www.googleapis.com/auth/classroom.rosters.readonly',
+      // View your course work and grades in Google Classroom
+      'https://www.googleapis.com/auth/classroom.student-submissions.me.readonly',
+
+      // View course work and grades for students in the Google Classroom classes you teach or administer
+      'https://www.googleapis.com/auth/classroom.student-submissions.students.readonly',
+
+      // See, create, and edit topics in Google Classroom
+
+      // 'https://www.googleapis.com/auth/classroom.topics',
+
+      /*   // View topics in Google Classroom */
+      'https://www.googleapis.com/auth/classroom.topics.readonly',
+    ].join(' '),
+  }
 };
